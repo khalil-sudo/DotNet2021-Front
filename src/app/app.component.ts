38e9ad15203f274a45eea3f54546dff05aa5e03c
khalil-sudo <72109796+khalil-sudo@users.noreply.github.com>
2021-06-05 22:04:54 +0100

    added input, stored to array, added array to display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
+import { variable } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'DotNet2021-Front';
+
+  idArray = [''];
+
+  addId(value: string) {
+    if (value != '') {
+      this.idArray.push(value);
+      console.log(this.idArray);
+    } else {
+      alert('Id Requis!!');
+    }
+  }
 }
