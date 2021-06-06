0470b25bc14149eff7d0170b40a2a69a1ca16e26
khalil-sudo <72109796+khalil-sudo@users.noreply.github.com>
2021-06-06 21:57:05 +0100

    added aniamtion and the angular animations dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { variable } from '@angular/compiler/src/output/output_ast';
 import { Component, ViewChild } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('moveInLeft', [
+      transition('void=> *', [
+        style({ transform: 'translateX(300px)' }),
+        animate(
+          200,
+          keyframes([
+            style({ transform: 'translateX(300px)' }),
+            style({ transform: 'translateX(0)' }),
+          ])
+        ),
+      ]),
+      transition('void=> *', [
+        style({ transform: 'translateX(0px)' }),
+        animate(
+          200,
+          keyframes([
+            style({ transform: 'translateX(0px)' }),
+            style({ transform: 'translateX(300px)' }),
+          ])
+        ),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'DotNet2021-Front';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
